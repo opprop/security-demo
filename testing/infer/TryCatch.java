b3732eeb01b29c439a7877963d571c005dcfbba6
d367wang <d367wang@uwaterloo.ca>
2021-04-27 10:00:55 -0400

    add stub file and test case

--- testing/infer/TryCatch.java
+++ testing/infer/TryCatch.java
@@ -1,12 +1,21 @@
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class TryCatch {
-    void foo(OutputStream os) {
+
+	private void writeFeatureVectors(OutputStream os) {
         try {
             os.write(255);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("IOException in method writeFeatureVectors: " + e.getMessage());
+
+            System.out.println("IOException in method writeFeatureVectors: " + e);
+
+            Logger.getLogger(TryCatch.class.getName()).log(Level.SEVERE, null, e);
+
+            throw new RuntimeException("can't continue execution due to fatal error", e);
         }
     }
 }
