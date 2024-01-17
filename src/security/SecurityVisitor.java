8dcd6384f6380b82215d6792f827b5060c988896
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-01-17 15:14:23 -0500

    redo import in security visitor

--- src/security/SecurityVisitor.java
+++ src/security/SecurityVisitor.java
@@ -2,21 +2,10 @@ package security;
 
 import checkers.inference.InferenceChecker;
 import checkers.inference.InferenceVisitor;
-import com.sun.source.tree.ExpressionTree;
-import com.sun.source.tree.MethodInvocationTree;
-import com.sun.source.tree.NewClassTree;
 import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
-import org.checkerframework.framework.type.AnnotatedTypeFactory;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
-import org.checkerframework.framework.type.AnnotatedTypeParameterBounds;
-import org.checkerframework.framework.util.AnnotatedTypes;
-import org.checkerframework.javacutil.ElementUtils;
-import org.checkerframework.javacutil.TreeUtils;
-import org.plumelib.util.CollectionsPlume;
 
 import javax.lang.model.element.ExecutableElement;
-import javax.lang.model.type.TypeKind;
-import java.util.List;
 
 public class SecurityVisitor extends InferenceVisitor<SecurityChecker, BaseAnnotatedTypeFactory> {
     public SecurityVisitor(
